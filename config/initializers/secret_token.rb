5a380403142ab8b23bf041e4a467fa594eb65237
William Notowidagdo <wnotowidagdo@gmail.com>
2013-11-09 17:37:04 +0530

    added - rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CalonApiEndpoint::Application.config.secret_key_base = '21525e14c0fd45b34cdb681617a3d1fd930270bf805e3567c1c607d0455b70d1ab10983a017ebcfd83573969d69afaadd52c6310488f5fa6c35dcb496521ff98'
