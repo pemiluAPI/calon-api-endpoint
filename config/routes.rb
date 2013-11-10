36902eae8a7e920c884eccf939b5583e2d39fb3c
William Notowidagdo <wnotowidagdo@gmail.com>
2013-11-10 18:07:16 +0700

    updated API route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 CalonApiEndpoint::Application.routes.draw do
-  mount Candidate::API => '/'
+  mount Pemilu::API => '/'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
